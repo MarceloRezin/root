ed13b057a9a51b902e6f8492431928ca4ab66494
Marcelo Rezin <marcelo.rezin@gmai.com>
2018-09-01 19:07:15 -0300

    Rota para view inicial

--- public/routes.js
+++ public/routes.js
@@ -1,10 +1,15 @@
 app.config(['$stateProvider', function($stateProvider) {
+
     $stateProvider
+        .state('index', {
+            url: 'index',
+            templateUrl: 'index-partial.html'
+        })
+
         .state('view1', {
             url: 'view1',
             templateUrl: 'views/view1/view1.html'
         })
 
         ;
-
 }]);
